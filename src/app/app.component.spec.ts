6fa5e8e48c00457eb408228391f105e7cc8e3a0f
Ioannis Drim <ioannisdrim@gmail.com>
2019-06-25 00:38:36 +0300

    master: scaffolding unit tests

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: []
+    }).overrideComponent(AppComponent, {
+      set: {
+        template: '<span></span>'
+      }
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
